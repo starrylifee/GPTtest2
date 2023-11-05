be0b9056133ed1eb4d66d9e92394b41c5c3865d6
starrylife <forinnocen@naver.com>
2023-11-05 18:08:06 +0900

    eighteen

--- pages/4_미술(작품분석전문가).py
+++ pages/4_미술(작품분석전문가).py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 
+# OpenAI API 키를 Streamlit secrets에서 가져옵니다.
 openai.api_key = st.secrets["api_key"]
 
 st.set_page_config(layout="wide")
@@ -32,29 +33,39 @@ if generate_button:
         st.warning("모든 필드를 채워주세요!")
     else:
         # 입력이 모두 채워졌을 때의 처리 로직
-        translation_prompt1 = f"{elements1}, {elements2}, {elements3}, {principles1}, {subject1}, {subject2}"
-        translation_prompt = f"{elements1}, {elements2}, {elements3}, {principles1}, {subject1}, {subject2} - 이 설명을 영어로 번역해주세요. 만일 초등학생에게 유해한 이미지를 요청할 경우에는 생성하지 말아주세요."
-        translation_response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=translation_prompt,
-            max_tokens=60,
-            temperature=0.5
-        )
-        
-        # 번역된 영어 텍스트를 가져옵니다.
-        translated_text = translation_response.choices[0].text.strip()
-        
-        # 이미지 생성을 위한 요청을 보냅니다 (DALL-E 엔진 사용).
-        image_response = openai.Completion.create(
-            prompt=translated_text,
-            n=1,
-            size="512x512"
-        )
-        
-        # 생성된 이미지의 URL을 가져옵니다.
-        image_url = image_response.choices[0].data['url']
-        
-        # 이미지를 화면에 표시합니다.
-        st.image(image_url, caption='여러분이 본 그림이 이 그림이 맞나요?')
-        st.write(translation_prompt1)
-        st.write(translated_text)
+        # 번역할 텍스트를 한국어에서 영어로 번역하는 로직
+        try:
+            translation_prompt = f"점, 선, 면: {elements1}, 질감: {elements2}, 공간: {elements3}, 균형/대비/강조/리듬/조화: {principles1}, 주제: {subject1}, 공간별 내용: {subject2} - 이 설명을 영어로 번역해주세요."
+            translation_response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=translation_prompt,
+                max_tokens=1024,
+                temperature=0.3
+            )
+            
+            # 번역된 영어 텍스트를 가져옵니다.
+            translated_text = translation_response.choices[0].text.strip()
+
+            # 이미지 생성 요청
+            image_response = openai.Image.create(
+                prompt=translated_text,
+                n=1,
+                size="1024x1024"
+            )
+            
+            # 생성된 이미지의 URL을 가져옵니다.
+            image_url = image_response.data[0]['url']
+            
+            # 이미지를 화면에 표시합니다.
+            st.image(image_url, caption='여러분이 본 그림이 이 그림이 맞나요?')
+            # 사용자가 입력한 내용을 화면에 표시합니다.
+            st.write("## 여러분이 입력한 분석 내용")
+            st.write(f"점, 선, 면: {elements1}")
+            st.write(f"질감: {elements2}")
+            st.write(f"공간: {elements3}")
+            st.write(f"균형/대비/강조/리듬/조화: {principles1}")
+            st.write(f"주제: {subject1}")
+            st.write(f"공간별 내용: {subject2}")
+            
+        except openai.error.OpenAIError as e:
+            st.error(f"API 요청 중 오류가 발생했습니다: {str(e)}")
