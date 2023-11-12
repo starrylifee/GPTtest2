df17e271c561aaa1f1e86ff20e19558649a2e675
starrylife <forinnocen@naver.com>
2023-11-12 17:59:59 +0900

    fortyfive

--- pages/4_미술(작품분석전문가).py
+++ pages/4_미술(작품분석전문가).py
@@ -1,5 +1,7 @@
 import streamlit as st
 import openai
+import requests
+from io import BytesIO
 
 # OpenAI API 키를 Streamlit secrets에서 가져옵니다.
 openai.api_key = st.secrets["api_key"]
@@ -38,7 +40,6 @@ if generate_button:
         st.warning("모든 필드를 채워주세요!")
     else:
         # 입력이 모두 채워졌을 때의 처리 로직
-        # 번역할 텍스트를 한국어에서 영어로 번역하는 로직
         try:
             translation_prompt = f"점, 선, 면: {elements1}, 질감: {elements2}, 공간: {elements3}, 균형/대비/강조/리듬/조화: {principles1}, 주제: {subject1}, 공간별 내용: {subject2} - 이 설명을 영어로 번역해주세요."
             translation_response = openai.Completion.create(
@@ -50,6 +51,10 @@ if generate_button:
             
             # 번역된 영어 텍스트를 가져옵니다.
             translated_text = translation_response.choices[0].text.strip()
+
+            # 번역된 텍스트를 숨겨진 상태로 표시
+            with st.expander("번역된 텍스트 보기"):
+                st.text(translated_text)
             
             # 이미지 생성 요청
             image_response = openai.Image.create(
@@ -59,18 +64,21 @@ if generate_button:
             )
             
             # 생성된 이미지의 URL을 가져옵니다.
-            image_url = image_response['data'][0]['url']
+            generated_image_url = image_response['data'][0]['url']
             
             # 이미지를 화면에 표시합니다.
-            st.image(image_url, caption='여러분이 본 그림이 이 그림이 맞나요?')
-            # 사용자가 입력한 내용을 화면에 표시합니다.
-            st.write("## 여러분이 입력한 분석 내용")
-            st.write(f"점, 선, 면: {elements1}")
-            st.write(f"질감: {elements2}")
-            st.write(f"공간: {elements3}")
-            st.write(f"균형/대비/강조/리듬/조화: {principles1}")
-            st.write(f"주제: {subject1}")
-            st.write(f"공간별 내용: {subject2}")
+            st.image(generated_image_url, caption='여러분이 본 그림이 이 그림이 맞나요?')
+
+            # 이미지 데이터를 다운로드하기
+            response = requests.get(generated_image_url)
+            image_bytes = BytesIO(response.content)
+
+            # 다운로드 버튼 추가
+            st.download_button(label="이미지 다운로드",
+                               data=image_bytes,
+                               file_name="generated_image.jpg",
+                               mime="image/jpeg")
             
         except openai.error.OpenAIError as e:
             st.error(f"API 요청 중 오류가 발생했습니다: {str(e)}")
+``
