69e5ab26e7897de1cfcd1d37675ede56abdb6f4a
starrylife <forinnocen@naver.com>
2023-11-13 02:24:39 +0900

    thirtyone

--- pages/6_국어(시화만들기).py
+++ pages/6_국어(시화만들기).py
@@ -13,6 +13,7 @@ st.header("시를 입력하고 시에 어울리는 그림을 생성해보세요"
 # 사용자 입력
 poem = st.text_area("시를 여기에 입력하세요.")
 subject = st.text_input("그림에 꼭 들어갔으면 하는 소재를 입력하세요.")
+style = st.selectbox("그림의 스타일을 선택하세요.", ["몽환적인", "사실적인", "만화스타일", "추상적인", "임프레셔니즘"])
 
 # 이미지 생성 버튼
 generate_button = st.button('이미지 생성')
@@ -22,9 +23,9 @@ if generate_button:
     if not poem or not subject:
         st.warning("시와 소재를 모두 입력해주세요!")
     else:
-        # 입력된 시와 소재를 영어로 번역하는 로직
+        # 입력된 시, 소재, 스타일을 영어로 번역하는 로직
         try:
-            translation_prompt = f"다음 시를 영어로 번역해주세요: {poem}\n\n그림에 꼭 들어갈 소재: {subject}"
+            translation_prompt = f"다음 시를 영어로 번역해주세요: {poem}\n\n그림에 꼭 들어갈 소재: {subject}\n\n선택한 그림 스타일: {style}"
             translation_response = openai.Completion.create(
                 engine="text-davinci-003",
                 prompt=translation_prompt,
@@ -46,7 +47,7 @@ if generate_button:
             image_url = image_response['data'][0]['url']
             
             # 이미지를 화면에 표시합니다.
-            st.image(image_url, caption='시와 소재에 기반한 이미지')
+            st.image(image_url, caption='시, 소재, 스타일에 기반한 이미지')
             
         except openai.error.OpenAIError as e:
             st.error(f"API 요청 중 오류가 발생했습니다: {str(e)}")
