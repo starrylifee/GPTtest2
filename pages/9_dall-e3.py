87db39bf6146c05b8eb3420c398bd5d68cd81989
starrylife <forinnocen@naver.com>
2023-11-18 18:30:46 +0900

    fiftysix

--- pages/9_dall-e3.py
+++ pages/9_dall-e3.py
@@ -6,6 +6,8 @@ from io import BytesIO
 # OpenAI API 키를 Streamlit secrets에서 가져옵니다.
 openai.api_key = st.secrets["api_key"]
 
+st.write("api_key")
+
 st.set_page_config(layout="wide")
 
 client = OpenAI()
