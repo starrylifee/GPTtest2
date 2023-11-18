abc68bdce906f9d456f34127be4457020bcd3e7b
starrylife <forinnocen@naver.com>
2023-11-18 22:13:38 +0900

    seventynine

--- pages/7_영어(자기소개타로카드).py
+++ pages/7_영어(자기소개타로카드).py
@@ -1,10 +1,10 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 import requests
 from io import BytesIO
 
-# Streamlit 시크릿으로부터 OpenAI API 키를 로드합니다.
-openai.api_key = st.secrets["api_key"]
+# OpenAI 객체 생성 시 API 키를 직접 제공
+client = OpenAI(api_key=st.secrets["api_key"])
 
 # 페이지 레이아웃을 설정합니다.
 st.set_page_config(layout="wide")
@@ -37,14 +37,20 @@ if password == correct_password:
             st.warning("모든 필드를 채워주세요!")
         else:
             # 이미지 생성 프롬프트를 생성합니다.
-            prompt = f"타로 카드 디자인으로 중앙에는 {gender}아이의 일러스트레이션이 있고, 주변에는 그 아이가 좋아하는 물건({likes_items}), 존경하는 사람({likes_people}), 장래희망({aspiration}), 지금 해보고 싶은 것({want_to_do}), 좋아하는 음식({favorite_food}), 좋아하는 나라({favorite_country}) 등이 둘러싸고 있습니다. 카드 하단에는 학생의 이름 '{name}'이 크게 표시됩니다."
+            prompt = f"타로 카드 디자인으로 중앙에는 {gender}아이의 일러스트레이션이 있고, 주변에는 그 아이가 좋아하는 물건({likes_items}), 좋아하는 사람({likes_people}), 장래희망({aspiration}), 지금 당장하고 싶은 것({want_to_do}), 좋아하는 음식({favorite_food}), 좋아하는 나라({favorite_country}) 등이 둘러싸고 있습니다. 카드 하단에는 학생의 이름 '{name}'이 크게 표시됩니다."
 
             try:
                 # OpenAI API를 호출하여 이미지를 생성합니다.
-                image_response = openai.Image.create(prompt=prompt, n=1, size="1024x1792")
+                image_response = client.images.generate(
+                    model="dall-e-3",
+                    prompt=prompt,
+                    size="1024x1792"
+                    quality="standard",
+                    n=1
+                )
 
                 # 생성된 이미지를 표시합니다.
-                generated_image_url = image_response['data'][0]['url']
+                generated_image_url = image_response.data[0].url
                 st.image(generated_image_url, caption=f"{name}의 개인화된 타로 카드")
 
                 # 이미지 다운로드를 위한 준비
@@ -57,7 +63,7 @@ if password == correct_password:
                                    file_name=f"{name}_tarot_card.jpg",
                                    mime="image/jpeg")
                 
-            except openai.error.OpenAIError as e:
-                st.error(f"API 요청 중 오류가 발생했습니다: {str(e)}")
+            except AttributeError as e:
+                st.error("An error occurred while accessing the response: " + str(e))
 else:
     st.warning("올바른 비밀번호를 입력해주세요.")
