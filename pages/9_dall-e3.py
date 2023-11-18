3efc8f2f15168f30e68c1e27535dce435e7b2377
starrylife <forinnocen@naver.com>
2023-11-18 18:49:03 +0900

    fiftyeight

--- pages/9_dall-e3.py
+++ pages/9_dall-e3.py
@@ -3,15 +3,12 @@ from openai import OpenAI
 import requests
 from io import BytesIO
 
-client = OpenAI(
-  organization=st.secrets["api_key"],
-)
-client.models.list()
+# OpenAI 객체 생성 시 API 키를 직접 제공
+client = OpenAI(api_key=st.secrets["api_key"])
 
 st.set_page_config(layout="wide")
 
-client = OpenAI()
-
+# 이미지 생성 요청
 response = client.images.generate(
   model="dall-e-3",
   prompt="a white siamese cat",
@@ -20,4 +17,8 @@ response = client.images.generate(
   n=1,
 )
 
-image_url = response.data[0].url
+# 응답에서 이미지 URL 추출 (API 업데이트에 따라 달라질 수 있음)
+image_url = response['data'][0]['url']  # 예시로, 실제 응답 구조에 맞춰야 함
+
+# 이미지 URL 출력
+st.image(image_url)
